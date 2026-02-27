d99129fd7a37691dbbabed5f6253649cec3bf737
lovelygarg607-ux <lovelygarg607@gmail.com>
2026-02-27 19:16:26 +0530

    Update app.js

--- Server/src/app.js
+++ Server/src/app.js
@@ -4,10 +4,17 @@ import adminrouter from "./route/adminroute.js"
 
 
 const app=express();
+ app.use(cors({
+    origin: [
+    "http://localhost:3000",
+    "http://localhost:8000",
+  " https://new-task-2-g3c8.onrender.com"
+    "https://new-task-gilt-eta.vercel.app/"
+  ],
+  credentials: true
+  
+ }))
 
-app.use(cors({
-   origin: "http://localhost:8000"
-}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"))
