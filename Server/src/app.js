0206f83297ffb5f6a0223aed929b5425df260bb7
lovelygarg607-ux <lovelygarg607@gmail.com>
2026-02-27 19:24:38 +0530

    Update app.js

--- Server/src/app.js
+++ Server/src/app.js
@@ -1,26 +1,23 @@
 import express from "express";
 import cors from "cors";
-import adminrouter from "./route/adminroute.js"
+import adminrouter from "./route/adminroute.js";
 
+const app = express();
 
-const app=express();
- app.use(cors({
-    origin: [
+app.use(cors({
+  origin: [
     "http://localhost:3000",
     "http://localhost:8000",
-  " https://new-task-2-g3c8.onrender.com"
-    "https://new-task-gilt-eta.vercel.app/"
+    "https://new-task-2-g3c8.onrender.com",
+    "https://new-task-gilt-eta.vercel.app"
   ],
   credentials: true
-  
- }))
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"))
-app.use("/store/admin", adminrouter)
+app.use(express.static("public"));
 
+app.use("/store/admin", adminrouter);
 
-
-
-export {app}
+export { app };
